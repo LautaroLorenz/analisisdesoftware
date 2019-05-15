067a66595399673e1333c0eee59f0bae89f139b4
mariano.riquelme@globallogic.com <mariano.riquelme@globallogic.com>
2019-05-15 09:12:28 -0300

    se sube avance de codigo (se agrego ventanas y logica)

---
+++ Herramienta_de_testing/HTesting/src/Funcion/Funcion.java
@@ -0,0 +1,162 @@
+package Funcion;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Funcion {
+	
+	public static  int CANTLINEAARCHIVO = 0;
+	public String namefuncion;
+	public int getCantlineaComentario() {
+		return cantlineaComentario;
+	}
+
+	private int numLiniaIni;
+	//private int numLineaFin;
+	//private int cantParam;
+	private int cantIf;
+	public int getNumLiniaIni() {
+		return numLiniaIni;
+	}
+
+	public int getLlaveCierreFun() {
+		return llaveCierreFun;
+	}
+
+	private int cantFor;
+	private int cantWhile;
+	private int cantlineaComentario;
+	private int llaveAperturaFun;
+	private int llaveCierreFun;
+	private int contadorLlaves;
+	private ArrayList<String> codigo;
+	
+	public Funcion(){
+		this.namefuncion = "";
+		this.numLiniaIni = 0;
+		//this.numLineaFin = 0;
+		this.cantIf = 0;
+		this.cantFor = 0;
+		this.cantWhile = 0;
+		this.cantlineaComentario = 0;
+		this.llaveAperturaFun =0;
+		this.llaveCierreFun = 0;
+		this.contadorLlaves = 0;
+		this.codigo = new ArrayList<String>();
+	}
+	
+	public static ArrayList<Funcion> getFuncionesArchivo(String archivo) throws FileNotFoundException{
+		
+		ArrayList<Funcion> listaFun = new ArrayList<Funcion>();
+		Funcion fun = null;
+		Scanner sc = new Scanner(new File(archivo));
+		String linea;
+		boolean cargandoFuncion = false;
+		
+		while(sc.hasNextLine()){
+			CANTLINEAARCHIVO++;
+			linea = sc.nextLine();
+			//linea = linea.trim();
+			
+			if(esFuncion(linea)  ){
+				fun = new Funcion();
+				//System.out.println(linea);
+				fun.codigo.add(linea);
+				fun.namefuncion = linea;
+				fun.namefuncion=fun.namefuncion.replace("{", "");
+				cargandoFuncion = true;
+				fun.numLiniaIni = CANTLINEAARCHIVO;
+				if(linea.contains("{")){
+					fun.llaveAperturaFun = CANTLINEAARCHIVO;
+				}
+					
+				
+			}
+			else{
+				if(cargandoFuncion){
+					fun.codigo.add(linea);
+					if(linea.contains("if("))
+						fun.cantIf++;
+					if(linea.contains("for("))
+						fun.cantFor++;
+					if(linea.contains("while("))
+						fun.cantWhile++;
+					if(linea.contains("/*") || linea.contains("*") || linea.contains("//") )
+						fun.cantlineaComentario++;
+				}
+					
+			}
+			
+			if(linea.contains("{")&&cargandoFuncion){
+				if(fun.contadorLlaves==0)
+					fun.llaveAperturaFun = CANTLINEAARCHIVO;
+				fun.contadorLlaves++;
+				
+			}
+			if(linea.contains("}") && cargandoFuncion){
+				fun.contadorLlaves--;
+				if(cargandoFuncion && fun.contadorLlaves==0){
+					cargandoFuncion = false;
+					listaFun.add(fun);
+					//System.out.println("se termino de cargar funcion");
+					fun.llaveCierreFun = CANTLINEAARCHIVO;
+				}
+			}
+			
+				
+			
+		}
+		
+		return listaFun;
+		
+	}
+	
+	
+	
+	public static boolean esFuncion(String linea){
+		
+		if(linea.contains("package") || linea.contains("import") || linea.contains("class") ||
+		   linea.contains("interface") || linea.contains("Enum") || linea.contains("/*") || 
+		   linea.contains("*/") || linea.startsWith("*", 1) )
+			// TODAVIA NO VALIDE EL TEMA DE LAS DELCARACION DE PARAMETROS CON LLAVE
+		return false;
+		if(linea.contains("(") && linea.contains(")") && 
+		  (linea.contains("public")|| linea.contains("private")))
+			return true;
+		else return false;
+
+		
+	}
+	
+	public int getComplejidadCiclomatica(){
+		return this.cantIf+this.cantWhile+this.cantFor+1;
+	}
+	
+	public static void mostrarFuncion(Funcion fun){
+		
+		System.out.println("=====================================================");
+		System.out.println("namefuncion: "+fun.namefuncion);
+	    System.out.println("numLiniaIni: "+fun.numLiniaIni);
+		//System.out.println("numLineaFin:"+fun.numLineaFin);
+		System.out.println("cantIf: "+fun.cantIf);
+		System.out.println("cantFor: "+fun.cantFor);
+		System.out.println("cantWhile: "+fun.cantWhile);
+		System.out.println("cantlineaComentario: "+fun.cantlineaComentario);
+		System.out.println("llaveAperturaFun: "+fun.llaveAperturaFun);
+		System.out.println("llaveCierreFun: "+fun.llaveCierreFun);
+		System.out.println("contadorLlaves:"+fun.contadorLlaves);
+		System.out.println("===============codigo==================================");
+		for(int  i= 0; i < fun.codigo.size();i++)
+			System.out.println(fun.codigo.get(i));
+
+		
+	}
+	
+		public ArrayList<String> getCodigo(){
+			return this.codigo;
+		 }
+}
---
+++ Herramienta_de_testing/HTesting/src/ventana/HerramientaDeTesting.java
@@ -0,0 +1,246 @@
+package ventana;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.List;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import Funcion.Funcion;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+
+public class HerramientaDeTesting extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private ArrayList<Funcion> funciones = new ArrayList<Funcion>();
+	private List listMetodos = new List();
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private final Action action = new SwingAction();;
+	String ruta;
+	private List codigo = new List();
+	String lim = "hola";
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HerramientaDeTesting frame = new HerramientaDeTesting();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HerramientaDeTesting() {
+		
+
+		setTitle("EQUIPO 7*****Herramienta de Testing");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 994, 704);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(122, 22, 846, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Metodos encontrados");
+		lblNewLabel.setBounds(76, 102, 129, 34);
+		
+		contentPane.add(lblNewLabel);
+		listMetodos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//codigo.add(listMetodos.getSelectedIndex());
+				codigo.removeAll();
+				
+				for(int i = 0; i< funciones.get(listMetodos.getSelectedIndex()).getCodigo().size();i++){
+					String linea = funciones.get(listMetodos.getSelectedIndex()).getCodigo().get(i);
+					
+					if(linea.contains("if") || linea.contains("while") || linea.contains("for")){
+						linea = linea.toUpperCase();
+						
+						
+					}
+				
+					codigo.add(linea);
+					
+				}
+				
+			}
+		});
+		
+		listMetodos.setBounds(10, 142, 409, 188);
+		contentPane.add(listMetodos);
+		
+		
+		
+		codigo.setBounds(10, 374, 546, 271);
+		contentPane.add(codigo);
+		
+		
+		//codigo.add(listMetodos.getSelectedIndex()+"");
+		/*
+		for(int i = 0; i< funciones.get(listMetodos.getSelectedIndex()).getCodigo().size();i++){
+			codigo.add(funciones.get(listMetodos.getSelectedIndex()).getCodigo().get(i));
+		}
+		*/
+		
+		
+		
+		JLabel lblNewLabel_1 = new JLabel("Analisis del metodo");
+		lblNewLabel_1.setBounds(731, 112, 163, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Lineas de codigo");
+		lblNewLabel_2.setBounds(568, 151, 100, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Lineas comentadas");
+		lblNewLabel_3.setBounds(549, 206, 119, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Porcentaje de codigo comentado");
+		lblNewLabel_4.setBounds(477, 258, 191, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Complejidad ciclomatica");
+		lblNewLabel_5.setBounds(518, 319, 150, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(749, 148, 86, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(749, 203, 86, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(749, 255, 86, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(749, 316, 86, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		
+		
+		
+		JButton btnNewButton = new JButton("Archivo");
+		btnNewButton.setBounds(10, 21, 89, 23);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					elegirArchivo();
+					textField.setText(ruta);
+					
+						
+					
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			
+		});
+		contentPane.add(btnNewButton);
+		
+		JLabel lblCodigoFuente = new JLabel("Codigo fuente");
+		lblCodigoFuente.setBounds(76, 336, 129, 34);
+		contentPane.add(lblCodigoFuente);
+		
+		JButton btnNewButton_1 = new JButton("Correr Analisis");
+		btnNewButton_1.setAction(action);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				calcularValoresAnalisis();
+			}
+		});
+		btnNewButton_1.setBounds(697, 456, 138, 77);
+		contentPane.add(btnNewButton_1);
+
+	}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "Correr Analisis");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	/*
+	 * LOGICA DE PANTALLA
+	 */
+	private void elegirArchivo() throws FileNotFoundException {
+		Scanner entrada = null;
+
+		JFileChooser selectorArchivos = new JFileChooser();
+	
+		FileNameExtensionFilter filtro = new FileNameExtensionFilter("Archivos Java", "java");
+		selectorArchivos.setFileFilter(filtro);
+		selectorArchivos.showOpenDialog(selectorArchivos);
+		
+		ruta = selectorArchivos.getSelectedFile().getAbsolutePath();
+		buscarMetodos(ruta);
+	}
+	
+	private void buscarMetodos(String ruta) throws FileNotFoundException {
+		
+		funciones = Funcion.getFuncionesArchivo(ruta);
+		
+		for (int j = 0; j < funciones.size(); j++) {
+			listMetodos.add(funciones.get(j).namefuncion);
+		}
+		
+	}
+	
+	private void calcularValoresAnalisis(){
+
+		int index = listMetodos.getSelectedIndex();
+		int lineasCodigo = funciones.get(index).getLlaveCierreFun()-funciones.get(index).getNumLiniaIni()+1;
+		double porcentajeCodigoComentado = (funciones.get(index).getCantlineaComentario()* 100)/lineasCodigo;
+		
+		textField_1.setText(lineasCodigo+"");
+		textField_2.setText(funciones.get(index).getCantlineaComentario()+"");
+		textField_3.setText(porcentajeCodigoComentado+"");
+		textField_4.setText(funciones.get(index).getComplejidadCiclomatica()+"");
+	}
+}
